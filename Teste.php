c277bd234e6ddbddf0ca52b07072dbfc2644865d
Paulo <paulo@local.com>
2018-11-22 11:49:55 -0300

    Initial commit

---
+++ Teste.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulo
+ * Date: 22/11/18
+ * Time: 11:23
+ */
+
+use Encryption\Encryption;
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'src/Encryption.php';
+
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+$encryption = new Encryption('teste', 'aes-128-cbc-hmac-sha256');
+
+echo $encryption->encript('Paulo');
+echo $encryption->decript('iDWyYKEELPyfmUk0Fzq94IOcnmAgUn/UQ5/n5C2ShVb3Sv3IQHHqI3sKcIyrazf9lR4dyYQszgJnPT0ioMlewQ==');
---
+++ src/Encryption.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulo
+ * Date: 22/11/18
+ * Time: 10:01
+ */
+
+namespace Encryption;
+
+class Encryption
+{
+    private $key;
+    private $cipher;
+
+    public function __construct($key, $cipher)
+    {
+        $this->key = $key;
+        $this->cipher = $cipher;
+    }
+
+    //Função de Encriptação
+
+    public function encript($textToEncript) {
+
+        $ivlen = openssl_cipher_iv_length($this->cipher);
+
+        $iv = openssl_random_pseudo_bytes($ivlen);
+
+        $ciphertext_raw = openssl_encrypt($textToEncript, $this->cipher, $this->key, $options = OPENSSL_RAW_DATA, $iv);
+
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
+
+        $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
+
+        return $ciphertext;
+
+    }
+
+    //Função de Decriptação
+
+    public function decript($textToDecript) {
+
+        $c = base64_decode($textToDecript);
+
+        $ivlen = openssl_cipher_iv_length($this->cipher);
+
+        $iv = substr($c, 0, $ivlen);
+
+        $hmac = substr($c, $ivlen, $sha2len = 32);
+
+        $ciphertext_raw = substr($c, $ivlen + $sha2len);
+
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $this->cipher, $this->key, $options = OPENSSL_RAW_DATA, $iv);
+
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
+
+        if (hash_equals($hmac, $calcmac))
+            return $original_plaintext;
+        else
+            return "decript error!";
+    }
+}
